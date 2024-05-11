06b302cdba2ea3e6fab0aaf9b121fdf334727f93
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-05-11 13:44:07 +0530

    Modify App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import Navbar from './components/Navbar'
+
 import Footer from './components/Footer'
 import Body from './pages/Body'
 import Contact from './components/Contact'
 import Subjects from './components/Subjects'
 import SignUpTemp from './pages/Authentication/SignUpTemp'
-import Login from './pages/Authentication/Login'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Admission from './components/Admission'
 import LoginTemp from './pages/Authentication/LoginTemp'
 import NavbarTemp from './components/NavbarTemp'
 import About from './components/About'
-
 function App () {
   return (
     <>
