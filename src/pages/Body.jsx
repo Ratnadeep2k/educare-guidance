dbb0e567a2cc1143aa28ad4761e475a989f045a6
mnz02 <abdulminhaz47@gmail.com>
2024-09-05 16:12:15 +0530

    Merged with main branch

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,16 +1,5 @@
 import React from 'react'
-import Carousel from '../components/Carousel'
-import Info from '../components/Info'
-import Notice from '../components/Notice/Notice'
-import Courses from '../components/Courses'
-import PopupForm from '../components/PopupForm'
-import Hero from '../app/Hero'
-import OutImpressiveStats from '../app/OurImpressiveStats'
-import CoursesCategories from '../app/CoursesCategories'
-import ExploreCourses from '../app/ExploreCourses'
-import Testimonial from '../app/Testimonial'
-import Events from '../app/Events'
-import StudentsFeedback from '../app/StudentsFeedback'
+import HomeTemp from '../components/HomeTemp/HomeTemp'
 function Body () {
   return (
     <div>
@@ -19,13 +8,7 @@ function Body () {
             <Info />
             <Notice />
             <Courses /> */}
-      <Hero />
-      <OutImpressiveStats />
-      <CoursesCategories />
-      <ExploreCourses />
-      <Testimonial />
-      <Events />
-      <StudentsFeedback />
+      <HomeTemp />
     </div>
   )
 }
